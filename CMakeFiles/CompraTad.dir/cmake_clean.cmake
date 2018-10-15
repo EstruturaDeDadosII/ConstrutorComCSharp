f810445f57199e645648e05645af864159b3f588
Thiago Cunha <thiagosantoscunha@outlook.com>
2018-10-15 11:19:17 -0300

    Adicionando exercicio

---
+++ CMakeFiles/CompraTad.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompraTad.dir/main.cpp.obj"
+  "CompraTad.pdb"
+  "CompraTad.exe"
+  "CompraTad.exe.manifest"
+  "libCompraTad.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompraTad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
